aa9bccd763879060132684bc83235387430f1d5f
VictorSundberg1 <victor.sundberg1@hotmail.com>
2025-03-24 14:40:24 +0100

    Initial Commit

---
+++ TranslateGameTests/TranslateGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateGameTests.swift
+//  TranslateGameTests
+//
+//  Created by Victor Sundberg on 2025-03-24.
+//
+
+import Testing
+@testable import TranslateGame
+
+struct TranslateGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
