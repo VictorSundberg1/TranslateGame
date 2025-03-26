47712364aa1324b8a472da8acd1921c53d7a25b1
Alexander <malmz90@gmail.com>
2025-03-26 10:47:19 +0100

    Timer, shakeeffect, start of tableview

--- TranslateGame/GameViewController.swift
+++ TranslateGame/GameViewController.swift
@@ -8,39 +8,39 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var currentWordLabel: UILabel!
     @IBOutlet weak var answerTextField: UITextField!
     
     let wordPairs = [
-            ["hej": "hello"],
-            ["tack": "thank you"],
-            ["ja": "yes"],
-            ["nej": "no"],
-            ["adjö": "goodbye"],
-            ["god morgon": "good morning"],
-            ["kvinna": "woman"],
-            ["man": "man"],
-            ["hus": "house"],
-            ["bil": "car"],
-            ["katt": "cat"],
-            ["hund": "dog"],
-            ["vatten": "water"],
-            ["bok": "book"],
-            ["mat": "food"],
-            ["bröd": "bread"],
-            ["mjölk": "milk"],
-            ["äta": "eat"],
-            ["dricka": "drink"],
-            ["springa": "run"],
-            ["gå": "walk"],
-            ["sova": "sleep"]
-        ]
+        ["hej": "hello"],
+        ["tack": "thank you"],
+        ["ja": "yes"],
+        ["nej": "no"],
+        ["adjö": "goodbye"],
+        ["god morgon": "good morning"],
+        ["kvinna": "woman"],
+        ["man": "man"],
+        ["hus": "house"],
+        ["bil": "car"],
+        ["katt": "cat"],
+        ["hund": "dog"],
+        ["vatten": "water"],
+        ["bok": "book"],
+        ["mat": "food"],
+        ["bröd": "bread"],
+        ["mjölk": "milk"],
+        ["äta": "eat"],
+        ["dricka": "drink"],
+        ["springa": "run"],
+        ["gå": "walk"],
+        ["sova": "sleep"]
+    ]
     
     var currentScore = 0
-    var time = 60
+    var time : Double = 10
     var timer: Timer?
     var currentWordPair:[String:String] = [:]
     
@@ -49,7 +49,7 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         startGame()
         // Do any additional setup after loading the view.
     }
@@ -57,7 +57,54 @@ class GameViewController: UIViewController {
     func startGame(){
         
         randomizeWord()
+        startTimer()
+    }
+    
+    
+    func startTimer(){
+        timer?.invalidate()
         
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true ) { [weak self] _ in
+        guard let self = self else { return }
+            
+            self.time -= 0.1
+            
+            self.timerLabel.text = "Time : \(Int(self.time))"
+            
+            if self.time <= 0 {
+                endGame()
+            }
+                        
+        }
+    }
+    
+    
+    func endGame(){
+        timer?.invalidate()
+        
+        let alert = UIAlertController(title: "Game Over", message: "Your score is \(currentScore)", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title:"Play again", style: .destructive, handler: { _ in self.dismiss(animated: true)}))
+        
+        alert.addAction(UIAlertAction(title: "High score", style: .default, handler: {_ in self.showHighscore()}))
+        
+        
+        present(alert, animated: true)
+    }
+    
+    func showHighscore (){
+        performSegue(withIdentifier: "showHighScore", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showHighScore"{
+            
+           if let destinationVC = segue.destination as? HighScoreViewController {
+               destinationVC.highScore = currentScore
+           }
+            
+         
+        }
     }
     
     func randomizeWord(){
@@ -72,8 +119,9 @@ class GameViewController: UIViewController {
     }
     
     func checkAnswer(_ translation:String) -> Bool {
+        
         if let correctAnswer = currentWordPair.values.first?.lowercased(){
-            return correctAnswer.lowercased() == correctAnswer
+            return translation.lowercased() == correctAnswer
         }
         return false
     }
@@ -82,9 +130,11 @@ class GameViewController: UIViewController {
     
     
     
-    @IBAction func enterButton(_ sender: UIButton) {
+    @IBAction func enterButton(_ sender: Any) {
         
         guard let translation = answerTextField.text, !translation.isEmpty else { return }
+        
+        print(translation)
         
         if checkAnswer(translation){
             currentScore += 1
@@ -92,13 +142,21 @@ class GameViewController: UIViewController {
             
             randomizeWord()
         } else {
-//            shakeTextField()
+           shakeTextField()
         }
         
         answerTextField.text = ""
         
     }
     
+    
+    func shakeTextField() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: .linear)
+        animation.duration = 0.6
+        animation.values = [-10.0, 10.0, -10.0, 10.0, -5.0, 5.0, -2.5, 2.5, 0.0]
+        answerTextField.layer.add(animation, forKey: "shake")
+    }
     
     @IBAction func resetGame(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
--- TranslateGame/HighScoreViewController.swift
+++ TranslateGame/HighScoreViewController.swift
@@ -7,15 +7,39 @@
 
 import UIKit
 
-class HighScoreViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class HighScoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var highScore = 0
+    var highScoreArray: [Int] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
+        
+        highScoreArray.append(highScore)
+        highScoreArray.sort(by: >)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return highScoreArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ScoreCell", for: indexPath)
+        cell.textLabel?.text = String(highScoreArray[indexPath.row])
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
