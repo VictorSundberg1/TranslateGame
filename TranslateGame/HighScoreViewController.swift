19563e23561a0dc7f006a4ae08de017a126995c7
Alexander <malmz90@gmail.com>
2025-03-27 14:09:58 +0100

    Animation new record

--- TranslateGame/HighScoreViewController.swift
+++ TranslateGame/HighScoreViewController.swift
@@ -16,6 +16,7 @@ class HighScoreViewController: UIViewController, UITableViewDelegate, UITableVie
     var highScore = 0
     var highScoreArray: [String] = []
     var playerName = ""
+    var isNewHighScore = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +26,7 @@ class HighScoreViewController: UIViewController, UITableViewDelegate, UITableVie
         let playerString = "\(highScore)p - \(playerName)"
         
         loadHighScore()
+        isNewHighScore = checkForNewHighScore()
         
         if playerName.isEmpty && highScore == 0{
             return
@@ -32,6 +34,10 @@ class HighScoreViewController: UIViewController, UITableViewDelegate, UITableVie
             highScoreArray.append(playerString)
             highScoreArray.sort(by: >)
             saveHighScore()
+            
+            if isNewHighScore {
+               showCelebrationAnimation()
+            }
         }
         
         
@@ -51,6 +57,57 @@ class HighScoreViewController: UIViewController, UITableViewDelegate, UITableVie
         let cell = tableView.dequeueReusableCell(withIdentifier: "ScoreCell", for: indexPath)
         cell.textLabel?.text = String(highScoreArray[indexPath.row])
         return cell
+    }
+    
+    func checkForNewHighScore() -> Bool {
+            
+            if highScoreArray.isEmpty && highScore > 0 {
+                return true
+            }
+            
+      
+        if let topScoreEntry = highScoreArray.first {
+        
+            if let scoreEndPosition = topScoreEntry.firstIndex(of: "p") {
+                let scoreText = topScoreEntry[..<scoreEndPosition]
+                        
+                if let existingTopScore = Int(scoreText) {
+
+                    return highScore > existingTopScore
+                }
+            }
+        }
+            
+            return false
+        }
+        
+    func showCelebrationAnimation() {
+        // Create a label for the celebration message
+        let celebrationLabel = UILabel()
+        celebrationLabel.text = "CONGRATS NEW RECORD! 🎉"
+        celebrationLabel.textAlignment = .center
+        celebrationLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        celebrationLabel.textColor = .white
+        celebrationLabel.backgroundColor = UIColor(red: 0.2, green: 0.6, blue: 1.0, alpha: 0.8)
+        celebrationLabel.layer.cornerRadius = 10
+        celebrationLabel.clipsToBounds = true
+        
+        // Increase the width from 250 to 350 and height from 50 to 60
+        celebrationLabel.frame = CGRect(x: 0, y: 0, width: 350, height: 60)
+        celebrationLabel.center = CGPoint(x: view.center.x, y: -50) // Start above the screen
+        view.addSubview(celebrationLabel)
+        
+        // Rest of the animation code remains the same
+        UIView.animate(withDuration: 0.7, delay: 0.3, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.8, options: [], animations: {
+            celebrationLabel.center = CGPoint(x: self.view.center.x, y: self.view.center.y - 100)
+        }, completion: { _ in
+            // After a delay, animate it back up
+            UIView.animate(withDuration: 0.5, delay: 2.0, options: [], animations: {
+                celebrationLabel.center = CGPoint(x: self.view.center.x, y: -50)
+            }, completion: { _ in
+                celebrationLabel.removeFromSuperview()
+            })
+        })
     }
      
     @IBAction func backButton(_ sender: UIButton) { self.dismiss(animated: true)
